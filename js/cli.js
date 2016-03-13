d98ca5214a174040519f7442fb09e9d09f972f00
Neil Larion <neil.larion@gmail.com>
2016-03-13 13:47:29 -0700

    Get browserify working

--- js/cli.js
+++ js/cli.js
@@ -1,7 +1,10 @@
 var pingPong = require('./ping-pong.js').pingPong;
+var prompt = require('prompt');
+prompt.start();
 
-console.log('Ping-ponging up to 100!');
-var result = pingPong(100);
-result.forEach(function(element) {
-  console.log(element);
+prompt.get('goal', function(err, result) {
+  var result = pingPong(result.goal);
+  result.forEach(function(element) {
+    console.log(element);
+  });
 });
